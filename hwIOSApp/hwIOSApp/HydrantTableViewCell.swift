144fef1ff4a71ef77819f73f6402a3979dba5bf7
Brian Nelson <brian@treegecko.com>
2015-11-30 21:45:54 -0700

    Work on Table view for nearby hydrants.

---
+++ hwIOSApp/hwIOSApp/HydrantTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  HydrantTableViewCell.swift
+//  hwIOSApp
+//
+//  Created by Brian Nelson on 11/30/15.
+//  Copyright © 2015 Brian Nelson. All rights reserved.
+//
+
+import UIKit
+
+class HydrantTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}

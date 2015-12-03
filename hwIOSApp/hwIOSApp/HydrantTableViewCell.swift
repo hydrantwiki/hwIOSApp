3997b546a7210ae6de012c31a138f48552636ecc
Brian Nelson <brian@treegecko.com>
2015-12-02 22:36:05 -0700

    Work towards getting hydrant table to load

--- hwIOSApp/hwIOSApp/HydrantTableViewCell.swift
+++ hwIOSApp/hwIOSApp/HydrantTableViewCell.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class HydrantTableViewCell: UITableViewCell {
 
+    @IBOutlet weak var DistanceLabel: UILabel!
+    @IBOutlet weak var LocationLabel: UILabel!
+    @IBOutlet weak var DetailsLabel: UILabel!
+    @IBOutlet weak var HydrantImage: UIImageView!
+        
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
